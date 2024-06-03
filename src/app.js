a5f876441ebf83e21fe95f142a1cc73b849ede13
GolamAhammed <devgolamgit@gmail.com>
2024-06-03 18:11:47 +0530

    changes on error response

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/erorr.middleware.js";
-
+import UserRouter from "./routes/user.routes.js" ;
 
 const app = express();
 
@@ -15,6 +15,14 @@ app.use(express.json({limit:"20kb"}));
 app.use(express.urlencoded({extended:true,limit:"20kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+//routes
+app.use("/api/v1/users", UserRouter)
+
+
+
+
 
 app.use(errorMiddleware);
 
--- src/middlewares/erorr.middleware.js
+++ src/middlewares/erorr.middleware.js
@@ -1,41 +1,45 @@
- import { ApiError } from "../utils/ApiError.js";
- 
- 
- const errorMiddleware = (err, req, res, next) => {
+import { ApiError } from "../utils/ApiError.js";
+
+const errorMiddleware = (err, req, res, next) => {
     let error = err;
+
     if (!(err instanceof ApiError)) {
-      const statusCode = err.statusCode || 500;
-      const message = err.message || "Internal Server Error";
-      error = new ApiError(statusCode, message, err.errors);
+        const statusCode = err.statusCode || 500;
+        const message = err.message || "Internal Server Error";
+        error = new ApiError(statusCode, message, err.errors);
     }
-  
+
     if (err.code === 11000) {
-      const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
-      error = new ApiError(400, message);
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+        error = new ApiError(400, message);
     }
+
     if (err.name === "JsonWebTokenError") {
-      const message = `Json Web Token is invalid, Try again!`;
-      error = new ApiError(400, message);
+        const message = `Json Web Token is invalid, Try again!`;
+        error = new ApiError(400, message);
     }
+
     if (err.name === "TokenExpiredError") {
-      const message = `Json Web Token is expired, Try again!`;
-      error = new ApiError(400, message);
+        const message = `Json Web Token is expired, Try again!`;
+        error = new ApiError(400, message);
     }
+
     if (err.name === "CastError") {
-      const message = `Invalid ${err.path}`;
-      error = new ApiError(400, message);
+        const message = `Invalid ${err.path}`;
+        error = new ApiError(400, message);
     }
-  
-    const errorMessage = error.errors
-      ? Object.values(error.errors)
-          .map((error) => error.message)
-          .join(" ")
-      : error.message;
-  
+
+    
+    const errorMessage = error.errors.length > 0
+        ? error.errors.map(err => err.message).join(" ")
+        : error.message || "An error occurred";
+
+   // console.error(err); //Log the error for debugging
+
     return res.status(error.statusCode).json({
-      success: false,
-      message: errorMessage,
+        success: false,
+        message: errorMessage,
     });
-  };
-  
-  export {errorMiddleware};
+};
+
+export { errorMiddleware };
