1d0cda34eb075d86b0197e1b7c6eea05f6e7ed8a
GolamAhammed <devgolamgit@gmail.com>
2024-06-10 17:35:56 +0530

    logout and login configuration

--- src/controllers/user.controlller.js
+++ src/controllers/user.controlller.js
@@ -2,8 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-
-
+import { generateAccessAndRefereshTokens } from "../utils/genarateToken.js";
 
 const registerUser = asyncHandler( async (req, res) => {
 
@@ -42,15 +41,85 @@ return res.status(201).json(
 new ApiResponse(200, createdUser,"User registered successfully")
 )
 
-})
+});
 
 const loginUser = asyncHandler(async (req, res) => { 
+    const {email,password} = req.body
+    
+    if(!email){
+        throw new ApiError(400,"email is required")
+    }
 
+  const user = await User.findOne({email})
+
+   if (!user){
+    throw new ApiError(400,"User dose not exist")
+   }
+
+  const isPasswordValid = await user.isPasswordCorrect(password)
+
+   if(!isPasswordValid) {
+    throw new ApiError(400,"Invalid password")
+   }
+
+  const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+ 
+
+ const loggedIn = await User.findById(user._id).select("-password -refreshToken")
+
+const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production'
+}
+
+return res
+.status(200)
+.cookie("accessToken", accessToken, options)
+.cookie("refreshToken", refreshToken, options)
+.json (
+    new ApiResponse(
+        200,
+        {
+            user: loggedIn, accessToken ,refreshToken 
+        },
+        "User loggedin successfully."
+    )
+)
+
+});
+
+const logoutUser = asyncHandler( async (req, res) => {
+ await  User.findByIdAndUpdate(
+    req.user._id,
+     {
+        $unset: {
+            refreshToken:1
+        }
+     },
+     {
+        new: true
+     }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production'
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+        new ApiResponse(200,{},"User logged Out")
+    )
 })
 
 export {
     registerUser,
     loginUser,
+    logoutUser
 };
 
 
