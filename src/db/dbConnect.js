5092623921c8af5cbc71d880c6ab9b504aa723c2
GolamAhammed <devgolamgit@gmail.com>
2024-10-26 23:01:12 +0530

    some spelling correction mongodb connection

--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -10,8 +10,9 @@ const connectDB = async () => {
 
        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
 
-       console.log(` MongoDB connected !! DB HOST:${connectionInstance.Connection.host}`);
+       console.log(`MongoDB connected !! DB HOST:${connectionInstance.connection.host}`);
 
+     
 
     } catch (error) {
         console.log("MONGODB connection error", error);
