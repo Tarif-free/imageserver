f2e3f2ffccc136b97f4b26d1fd0dee98b1511773
GolamAhammed <devgolamgit@gmail.com>
2024-11-13 18:59:06 +0530

    minor changes in stack if in productions and devlopment

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -13,10 +13,10 @@ class ApiError extends Error {
         this.errors = errors;
 
 
-        if (stack) {
-            this.stack = stack
-        } else {
-            Error.captureStackTrace(this,this.constructor)
+        // Only include the stack trace in non-production environments
+
+        if (process.env.NODE_ENV !== 'production') {
+            this.stack = stack || Error.captureStackTrace(this, this.constructor);
         }
     }
 }
